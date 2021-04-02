2b4c347c744f5e3bd073034511b5fecaeeafac17
fahim-cmd <hfahim087@gmail.com>
2021-04-02 23:20:26 +0600

    finally done

--- index.js
+++ index.js
@@ -9,37 +9,35 @@ const app = express()
 
 app.use(cors());
 app.use(bodyParser.json());
+
 const port = process.env.PORT || 5050;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omrot.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  console.log(err)
+  
   const bicycleCollection = client.db("bicycle").collection("products");
   const newBicycleCollection = client.db("bicycle").collection("newBicycle");
 
   app.get('/products', (req, res) => {
-      bicycleCollection.find()
+    bicycleCollection.find()
       .toArray((err, items) => {
-          res.send(items)
-          console.log('itemssss', items)
+        res.send(items)
+        console.log('itemssss', items)
       })
   })
 
-  //for delete item
+  
   app.get('/', (req, res) => {
-    
     bicycleCollection.find({})
-    .toArray((err, items) => {
-      res.send(items)
-    })
+      .toArray((err, items) => {
+        res.send(items)
+      })
   })
 
 
@@ -47,49 +45,40 @@ client.connect(err => {
   app.post('/addOrders', (req, res) => {
     const newOrders = req.body;
     newBicycleCollection.insertOne(newOrders)
-    .then(result => {
-      res.send(result.insertedCount > 0)
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
     console.log(newOrders);
   })
 
   //add order read in client side
   app.get('/orderRead', (req, res) => {
-    newBicycleCollection.find({email: req.query.email})
-    .toArray((err, documents) => {
-      res.send(documents)
-    })
+    newBicycleCollection.find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
   })
 
 
   app.post('/addProduct', (req, res) => {
-      const newProduct = req.body;
-      console.log('elee', newProduct)
+    const newProduct = req.body;
+    console.log('elee', newProduct)
 
-      bicycleCollection.insertOne(newProduct)
+    bicycleCollection.insertOne(newProduct)
       .then(result => {
-          console.log('server response', result)
-          res.send(result.insertedCount > 0)
+        console.log('server response', result)
+        res.send(result.insertedCount > 0)
       })
-  }) 
-
-  
-//delete method
-
-  app.delete('/delete/:id', (req, res) => {
-    // console.log(req.params.id)
-    bicycleCollection.deleteOne({_id: ObjectId(req.params.id)})
-    .then(result => {
-      
-      res.send(result.deletedCount > 0)
-    })
-
   })
 
+
+  //delete method
+  app.delete('/delete/:id', (req, res) => {    
+    bicycleCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0)
+      })
+  })
 });
-
-
-
-
 
 app.listen(process.env.PORT || port)
