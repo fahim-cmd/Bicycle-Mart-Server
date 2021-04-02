166b6e2fbacf5862373f2b6b4cc1cbf534aed263
fahim-cmd <hfahim087@gmail.com>
2021-04-02 22:41:41 +0600

    err write

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
+  console.log(err)
   const bicycleCollection = client.db("bicycle").collection("products");
   const newBicycleCollection = client.db("bicycle").collection("newBicycle");
 
